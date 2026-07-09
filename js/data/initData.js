77fd2bc18a26cf98bb005902ff1a4758d05f440c
mikethompson100 <mikethompson100@hotmail.com>
2026-07-09 18:08:21 -0400

    In progress.. setup form page to receive initial user data.

--- js/data/initData.js
+++ js/data/initData.js
@@ -1,4 +1,5 @@
-//
+// Determine if localStorage needs to be initialized for a new user or if data already exists
+let data = null;
 
 async function ensureUserDataExists() {
   const existing = localStorage.getItem("users");
@@ -19,20 +20,33 @@ async function ensureUserDataExists() {
     if (!data || typeof data !== "object") {
       throw new Error("Invalid schema data received");
     }
-
+    
     localStorage.setItem("users", JSON.stringify(data));
-    showNewUserForm(data);
-    return data;
+    showNewUserForm();
+    return;
+
   } catch (error) {
     console.log(error.message);
     throw error;
   }
 }
 
-ensureUserDataExists().catch((err) => {
+ensureUserDataExists(data).catch((err) => {
   console.error("Initialization error:", err.message);
 });
 
 function showNewUserForm() {
+      document.getElementById("form-container").style.display = "flex";
+      document.getElementById("main-container").style.display = "none";
 
+      const form = document.getElementById("form-container");
+      form.addEventListener('submit', (e) => {
+        const nameInput = document.getElementById("nameInput").value;
+        const goalInput = document.getElementById("goalInput").value;
+        let data = JSON.parse(localStorage.getItem("users"));
+          data.users[0].profile.name = nameInput; 
+          data.users[0].profile.overallGoal = goalInput; 
+          localStorage.setItem("users", JSON.stringify(data));
+      })
 }
+
